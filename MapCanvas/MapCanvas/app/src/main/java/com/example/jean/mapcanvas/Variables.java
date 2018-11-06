d0abd5dda43b735b9833e2da67e9b88b97dbf12c
SongYunShin <33708814+SongYunShin@users.noreply.github.com>
2018-11-06 17:39:14 +0900

    first commit - MapCanvas 1106

---
+++ MapCanvas/MapCanvas/app/src/main/java/com/example/jean/mapcanvas/Variables.java
@@ -0,0 +1,9 @@
+package com.example.jean.mapcanvas;
+
+/**
+ * Created by jean on 06/11/2018.
+ */
+
+public class Variables {
+    public static double a=0, b=0, result=0,radianConst=3.15192/180,theta=0,one_fourth_rad=90*radianConst,half_rad=180*radianConst,three_fourth=270*radianConst;
+}
