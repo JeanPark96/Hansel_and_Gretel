c82901dbcf6d04d9056426babd2ea40f8047b68e
JeanPark96 <hlpark7@naver.com>
2018-11-17 17:30:04 +0900

    committed by Park succeeded bitmap grid erased MapCanvas duplicate Directory

--- MapCanvas/MapCanvas/app/src/main/java/com/example/jean/mapcanvas/StepValue.java
+++
@@ -1,9 +0,0 @@
-package com.example.jean.mapcanvas;
-
-/**
- * Created by jean on 2018. 9. 9..
- */
-
-public class StepValue {
-    public static int Step=0;
-}
--- MapCanvas/MapCanvas/app/src/main/java/com/example/jean/mapcanvas/Variables.java
+++
@@ -1,9 +0,0 @@
-package com.example.jean.mapcanvas;
-
-/**
- * Created by jean on 06/11/2018.
- */
-
-public class Variables {
-    public static double a=0, b=0, result=0,radianConst=3.15192/180,theta=0,one_fourth_rad=90*radianConst,half_rad=180*radianConst,three_fourth=270*radianConst;
-}
