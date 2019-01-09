2d9a9ba169d38cb3c128710f0814d2aa1eccd54e
SongYunShin <33708814+SongYunShin@users.noreply.github.com>
2019-01-10 01:49:18 +0900

    +add missed part by Shin

--- app/src/main/java/com/example/jean/mapcanvas/drawCanvas.java
+++ app/src/main/java/com/example/jean/mapcanvas/drawCanvas.java
@@ -1,5 +1,7 @@
 package com.example.jean.mapcanvas;
 
+import android.graphics.Matrix;
+import android.view.View;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -11,10 +13,16 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class drawCanvas extends View {
-
-    private Bitmap drawBitmap,firstBitmap,bitmap;
+    public static File tempFile;
+    public static Bitmap drawBitmap, firstBitmap;
     private Paint paintCanvas,paintMark,canvasPaint, intermediatePaint;
     private Canvas canvas;
     ImageView canvasMap;
@@ -27,11 +35,10 @@ public class drawCanvas extends View {
     private int width,height;
     private Path path=new Path();
 
-
     public drawCanvas(Context context, AttributeSet attrs) {
         super(context,attrs);
-        paintCanvas=new Paint();
-        paintMark=new Paint();
+        paintCanvas = new Paint();
+        paintMark = new Paint();
         intermediatePaint = new Paint();
 
         paintCanvas.setStyle(Paint.Style.STROKE);
@@ -53,17 +60,17 @@ public class drawCanvas extends View {
         intermediatePaint.setAntiAlias(true);
         intermediatePaint.setStrokeJoin(Paint.Join.ROUND);
         // canvasMap=(ImageView)findViewById(R.id.grid_img);
-
     }
+
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        drawBitmap=Bitmap.createBitmap(getWidth(),getHeight(),Bitmap.Config.ARGB_8888);
-        canvas=new Canvas(drawBitmap);
-        width=getWidth();
-        height=getHeight();
-        firstBitmap= Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.grid),900,900,false);
-
+        drawBitmap = Bitmap.createBitmap(getWidth(),getHeight(),Bitmap.Config.ARGB_8888); //CanvasBitmap를 그대로 가져오면서 흑백처리 해버림.
+        //drawBitmap=Bitmap.createBitmap(getWidth(),getHeight(),Bitmap.Config.ARGB_8888);
+        canvas = new Canvas(drawBitmap);//이거 없으면 start 버튼 누르면 grid 이미지 사라짐
+        width = getWidth();
+        height = getHeight();
+        firstBitmap = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(),R.drawable.grid),900,900,false);
     }
 
     public void drawing(float azimuth,int msg) {
@@ -80,13 +87,13 @@ public class drawCanvas extends View {
             theta = r_azimuth;
             path.lineTo(endX,endY);
             canvas.drawOval(startX-8,startY-8,startX+8,endY+8,paintMark);//스타트 마크, 여기에 있어야 사라지지 않음
-
+            canvas.drawPath(path,paintCanvas);
         } else {
-
             angleDiff=r_azimuth - theta;
             angleDiffRadian =modifyDirection(angleDiff);
             updateLocation(startX,startY,angleDiffRadian);
             path.lineTo(endX,endY);
+            canvas.drawPath(path,paintCanvas);
 
             if(r_msg % 80 == 0) {
                 canvas.drawOval(startX-8, startY-8, startX+8, endY+8, intermediatePaint);
@@ -98,26 +105,70 @@ public class drawCanvas extends View {
         startX=endX;
         startY=endY;
     }
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
-        canvas.drawBitmap(firstBitmap,0,100,canvasPaint);
-
+        //firstBitmap= BitmapFactory.decodeResource(getResources(),R.drawable.grid);//res 폴더에 저장된 이미지를 Bitmap으로 만들때 사용함
+        //canvasMap.setImageBitmap(firstBitmap);
+        canvas.drawBitmap(firstBitmap,0,200,canvasPaint);
         canvas.drawBitmap(drawBitmap,0,0,canvasPaint);
-        canvas.drawPath(path,paintCanvas);
+
+        //canvasMap.setImageDrawable(new BitmapDrawable(getResources(),drawBitmap));//이거 없으면 안그려짐 애초에 그려질 수 있도록 drawBitmap이 없는것처럼 인식됨
+        //canvas.drawLine(startx,starty,endx,endy,paintCanvas);
+        //canvas.save();
     }
 
     public void finished(){
         paintMark.setColor(Color.RED);
         canvas.drawOval(startX-8,startY-8,startX+8,endY+8,paintMark);
+        //pathMark.addArc(startx-5,starty-5,startx+5,endy+5,0,360);
+        //  canvas.drawPath(pathMark,paintMark);
         invalidate();
+    }
+
+    /*public Bitmap RotateBitmap(Bitmap bitmap){
+        Matrix matrix = new Matrix();
+        matrix.setScale(1, -1); //상하반전
+        matrix.setScale(-1, 1); //좌우반전
+        //matrix.postRotate(180);
+
+        drawBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, false);
+
+        return drawBitmap;
+    }
+    public drawCanvas RotateBitmap(drawCanvas canvas){
+        canvas.setRotation(180);
+        return  canvas;
+    }*/
+
+    public static String saveBitmap(Context context, Bitmap bitmap, String name){
+        File storage = context.getCacheDir(); // 이 부분이 임시파일 저장 경로
+        String fileName = name + ".jpg";  // 파일이름은 마음대로!
+        tempFile = new File(storage, fileName);
+
+        /*if(tempFile.exists()){
+            drawBitmap = BitmapFactory.decodeFile(tempFile.getAbsolutePath());
+            TestActivity.imageView.setImageBitmap(drawBitmap);
+        }*/
+        try{
+            tempFile.createNewFile();  // 파일을 생성해주고
+            FileOutputStream out = new FileOutputStream(tempFile);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 90 , out);  // 넘거 받은 bitmap을 jpeg(손실압축)으로 저장해줌
+            out.flush();
+            out.close(); // 마무리로 닫아줍니다.
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return tempFile.getAbsolutePath();   // 임시파일 저장경로를 리턴해주면 끝!
     }
 
     private float modifyDirection(double angleDiff){
         if (angleDiff < 0)//angleDiff 값은 무조건 양수가 나오도록 보정
             angleDiff += 360;
-
 
         if(angleDiff>345 || angleDiff<=15){//방위각 보정
             angleDiff=0;
@@ -146,8 +197,6 @@ public class drawCanvas extends View {
         }
 
         angleDiffRadian = (float) (angleDiff* radianConst);//346~15는 직선 +16~45는 30도 오른쪽, 46~75는 60도 오른쪽, 76~105는 90도 오른쪽, 106~135는 120도, 136~165는 150도, 166~195는 180도,196~225는 210도, 226~255는 240도, 256~285는 270도 , 286~315는 300도, 316~345는 330도,
-
-
         return angleDiffRadian;
     }
 
