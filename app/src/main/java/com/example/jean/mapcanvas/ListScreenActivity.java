d404fa3b04f34b15c93ff09463aff7c49ade7115
Hyoneeing <36870725+Hyoneeing@users.noreply.github.com>
2019-01-17 17:39:38 +0900

    UI start again       by Lee

--- app/src/main/java/com/example/jean/mapcanvas/ListScreenActivity.java
+++ app/src/main/java/com/example/jean/mapcanvas/ListScreenActivity.java
@@ -1,8 +1,10 @@
 package com.example.jean.mapcanvas;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by i on 2019-01-17.
  */
 
-public class ListScreenActivity {
+public class ListScreenActivity extends AppCompatActivity{
 }
--- app/src/main/java/com/example/jean/mapcanvas/StartScreenActivity.java
+++ app/src/main/java/com/example/jean/mapcanvas/StartScreenActivity.java
@@ -1,8 +1,24 @@
 package com.example.jean.mapcanvas;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
 /**
  * Created by i on 2019-01-17.
  */
 
-public class StartScreenActivity {
+public class StartScreenActivity extends AppCompatActivity {
+    ImageView Hansel_and_Gretel_IMG;
+    ImageView Hansel_and_Gretel_TITLE;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startscreen);
+
+        Hansel_and_Gretel_IMG = findViewById(R.id.image);
+        Hansel_and_Gretel_TITLE = findViewById(R.id.title);
+    }
 }
